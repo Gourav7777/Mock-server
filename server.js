88caf6052b0f64e0c9e51cc8fcfc352b0a6fd66e
Gourav7777 <360inside360@gmail.com>
2022-11-30 19:40:47 +0530

    abcdefg

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ const port = process.env.PORT || 3001; // you can use any port number here; i ch
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port,()=>{
+    console.log("Code is running successfully")
+});
